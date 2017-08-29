222a29e9be7002867200dae9c71cf59f6cca5f24
Pineapple-Dynamics <takumawatanabe82@gmail.com>
2017-08-29 12:06:38 +0900

    devise omniouth facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :omniouthable, :omniouth_providers => [:facebook] :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
